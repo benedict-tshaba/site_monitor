a5ca0344f01301d7e096aa59056c5970065d9b06
Benedict Tshaba <benedicttshaba@gmail.com>
2017-06-08 15:20:14 +0200

    Add md5 and save_checksum functions

--- src/site_monitor.py
+++ src/site_monitor.py
@@ -1,10 +1,15 @@
 from threading import Lock, Thread
+import pickle
+import time
 
 file_lock = Lock()
+hashdict = {}
+hashfile = "logs/hashes.txt"
+changesf = "logs/website_changes.log"
 
 def log(msg):
 	with file_lock:
-		open('logs/website_changes.log', 'w') as f:
+		open(changesf, 'w') as f:
 			f.write(msg)
 
 def monitor(webpage):
@@ -12,16 +17,44 @@ def monitor(webpage):
 
 	while True:
 		changes = check_for_change(wepage)
-		if changes:
+		if changes not None:
 			log(changes)
+    return None
 
 def check_defacement(webpage):
 	""" Computes a checksum of the webpage and stores it, on subsequent calls it computes a checksum again and compares it with the one stored on disk if they dont match. Then Possibly the wepage has been defaced."""
+    pagehash = picle.load(hashfile)
+    if not pagehash[webpage]:
+        page = md5(webpage)
+        save_checksum(webpage, page)
+    
+    if md5(webpage) == pagehash[webpage]:
+        return None
 
-	return True
+    else:
+        msg = "Date: ".time.asctime()
+        msg .= "\nThe following ".webpage." might have been maliciosly modified"
+        msg .= "\nPlease verify and restore from backup"
+    
+	return None
+
+def save_checksum(filename, filehash):
+    hashdict[filename] = filehash
+    with open(hashfile) as f:
+        #f.write(hashdict)
+        pickle.dump(f,hashdict)
+    return None
+
+def md5(filename):
+    """ returns a hash of the filename given in the argument"""
+	filehash = hashlib.sha1()
+	with open(filename, "rb") as f:
+		for block in iter(lambda: f.read(4096), b""):
+			filehash.update(block)
+	return filehash.hexdigest()
 
 if __name__ == "__main__":
-	webpages = ['http://benedict.heliohost.org/', ...]
+	webpages = ['http://benedict.heliohost.org/index.html', 'http://benedict.heliohost.org/about.html', 'http://benedict.heliohost.org/', 'http://benedict.heliohost.org/services.html', ...]
 	for webpage in webpages:
 		checker = Thread(monitor, webpage)
 		checker.start()
--- tests/site_monitor_test.py
+++ tests/site_monitor_test.py
@@ -1,2 +1,14 @@
+import sys
+from os import path
+sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
+from ..src.site_monitor import monitor
 import unittest
 
+if __name__ == '__main__':
+    if __package__ is None:
+        import sys
+        from os import path
+        sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
+        from src.site_monitor import monitor
+    else:
+        from ..src.site_monitor import monitor
