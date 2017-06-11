730f8f54f9cc3efd9b45772fd971c200d39dee94
Benedict Tshaba <benedicttshaba@gmail.com>
2017-06-11 10:17:43 +0200

    Fix indentation

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 python -m unittest tests.site_monitor_test
+>logs/website_changes.log
--- tests/site_monitor_test.py
+++ tests/site_monitor_test.py
@@ -1,20 +1,22 @@
-from src.site_monitor_lib import SiteMon
+from src.lib.site_monitor_lib import SiteMon
+import time
 import unittest
 
+sm = SiteMon("logs/hashes.txt", "logs/website_changes.log")
+
 class SiteMonitorTestCase(unittest.TestCase):
-	sm = SiteMon("logs/hashes.txt", "logs/website_changes.log")
 
-    def test_hasher(self):
-        with open('tests/test_data/index.html', 'r') as f:
-		data = f.read()
-	self.assertEqual(sm.hasher(data), "d6ce37718229eaac245a11764d5f4850cca2ec52")
+	def test_hasher(self):
+		with open('tests/test_data/index.html', 'r') as f:
+			data = f.read()
+			self.assertEqual(sm.hasher(data), "d6ce37718229eaac245a11764d5f4850cca2ec52")
 
-    def test_log(self):
-	msg = " -- Test message check -- \n"
-	sm.log(msg)
-        with open('logs/website_changes.log', 'r') as f:
-		file_msg = f.readline()
-	self.assertEqual(msg, file_msg)
+	def test_log(self):
+		msg = " -- Test message check -- \n"
+		sm.log(msg)
+		with open('logs/website_changes.log', 'r') as f:
+			log_msg = f.readline()
+			self.assertEqual("Date: "+time.asctime()+msg, log_msg) # the log was written
 
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
