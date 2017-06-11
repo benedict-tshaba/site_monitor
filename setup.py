9e605aaef2caf58ed8fa8b08711d99cf8d33b910
Benedict Tshaba <benedicttshaba@gmail.com>
2017-06-11 17:51:01 +0200

    Update version

--- setup.py
+++ setup.py
@@ -2,14 +2,21 @@
 
 from distutils.core import setup
 
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
 setup(name='sitemon',
       scripts=['bin/sitemon'],
       version='1.0.3',
       description='A website monitoring program',
+      long_description=readme,
       author='Tshaba Phomolo Benedict',
       author_email='benedicttshaba@gmail.com',
       url='http://www.benedict.heliohost.org/',
       packages=['src','src/lib'],
       include_package_data = True,
-      license = "GPLv3",
+      license = license,
      )
