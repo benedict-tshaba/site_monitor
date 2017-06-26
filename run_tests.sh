9edc0b2bbe43687eb1aa0a7bea37612f1ada99f2
Tshaba Phomolo Benedict <phomolo@debian>
2017-06-26 14:57:42 +0200

    Fix bug

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 python -m unittest tests.site_monitor_test
->logs/website_changes.log
->logs/hashes.txt
+rm logs/website_changes.log logs/hashes.db
+
+touch logs/website_changes.log
+touch logs/hashes.db
