89f67d61d5df21bf4196b09c097be2f4c931628c
Tshaba Phomolo Benedict <phomolo@debian>
2017-06-26 14:45:03 +0200

    Finish adding exception handling code

--- lib/site_monitor_lib.py
+++ lib/site_monitor_lib.py
@@ -7,14 +7,15 @@ import pickle
 import time
 
 file_lock = Lock()
-hash_dict = {}
 
 class SiteMon(object):
 	def __init__(self, hash_file, changes_file):
 		self.hash_file = hash_file
 		self.changes_file = changes_file
+		self.hash_dict = {}
 
-	# logging changes to disk 
+
+	# logs changes to disk 
 	def log(self, msg):
 		""" Writes logs to disk, log messages passed (msg) should be in the following format:
 		They must be succeeded by a newline character: e.g: 'This is my log message\n' """
@@ -28,17 +29,17 @@ class SiteMon(object):
 	def hasher(self, data):
 		""" returns a hash of the file contents given in the argument"""
 
-		file_hash = hashlib.sha1()
+		file_hash = hashlib.sha256() #perhaps upgrade to sha512 for more security
 		file_hash.update(data)
 		return file_hash.hexdigest()
 
 	def save_checksum(self, filename, filehash):
 		"""Computes checksum of file, stores it in a dictionary and saves it to disk"""
 
-		hash_dict[filename] = filehash
+		self.hash_dict[filename] = filehash
 
-		with open(self.hash_file, 'w') as f:
-			pickle.dump(hash_dict, f)
+		with open(self.hash_file, 'a') as f:
+			pickle.dump(self.hash_dict, f)
 
 		return None
 
@@ -46,7 +47,6 @@ class SiteMon(object):
 		"""returns the contents of the log file"""
 		with open(self.changes_file) as f:
 			return f.read()
-		return None
 
 	# functions for checking the state of my website follow bellow
 	def check_defacement(self, pagename, data):
@@ -57,21 +57,23 @@ class SiteMon(object):
 			f = file(self.hash_file, "rb")
 			known_page_hash = pickle.load(f)
 			f.close()
-		except: # I should probably handle this exception, but its usually caused by the file not existing.
-			return None #This means we wont log anything, perhaps I should change it later.
+		except: 
+			msg = " - Unable to open the hash database"
+			return msg
 
 		try:
-			if pagename in known_page_hash:
-				if self.hasher(data) != known_page_hash[pagename]:
-					msg = "\nThe following "+webpage+" might have been maliciosly modified"
-					msg += " - Please verify and restore from backup"
-					return msg
+			if self.hasher(data) != known_page_hash[pagename]:
+				msg = "\nThe following "+webpage+" might have been maliciosly modified"
+				msg += " - Please verify and restore from backup"
+				return msg
 
 		except KeyError: #the page hash is not in our database
 			new_page_hash = self.hasher(data)
 			self.save_checksum(pagename, new_page_hash)
-			msg = "The page was not in the database, but has since been added"
+			msg = " - The page was not in the database, but has since been added"
 			return msg
+
+		return None
 
 	def check_availability(self, webpage):
 		"""returns a webpage discriptor if page is accessibly, None otherwise"""
